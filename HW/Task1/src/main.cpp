66cf64e1363bc6d7b10665bcc4cfab7e4ddfbc61
M1SH4-cmd <6el0zu61979@mail.ru>
2024-12-14 02:32:04 +0700

    Task1 done

--- HW/Task1/src/main.cpp
+++ HW/Task1/src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+void swapNums(int* a, int* b){
+    int buffer = *a;
+    a = b;
+    b = &buffer;
+    cout << "a:\t" << *a << endl << "b:\t" << *b << "\0";
+}
+
+
+int main(){
+
+    int a = 10;
+    int b = 20;
+
+    swapNums(&a, &b);
+
 }
