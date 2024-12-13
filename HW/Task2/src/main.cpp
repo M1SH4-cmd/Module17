14598a1d8e5df5703aab73669cd894c3799f5dfe
M1SH4-cmd <6el0zu61979@mail.ru>
2024-12-14 03:11:41 +0700

    Task2 done

--- HW/Task2/src/main.cpp
+++ HW/Task2/src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+void reversed(int* arr){
+    int i = 9;
+    while(i >= 0){
+        cout << *(arr + i) << " ";
+        i--;
+    }
+}
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    reversed(arr);
 }
